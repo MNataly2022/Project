adb41a9d3c3f646ac68673a8fcbee6cfca360f0c
Малова Наталия <nmalova@mail.ru>
2022-10-23 15:00:04 +0300

    .\Realize task

--- task/Program.cs
+++ task/Program.cs
@@ -1,25 +1,27 @@
-﻿// // Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
 // длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
 string[] array = { "hello", "2", "world", ":-)" };
-Console.Write("Массив с элементами меньше 3-х символов:\n[\"");
 
-bool find = false;
+string[] addElement(string[] array, string newElement)
+{
+    string[] newArray = new string[array.Length + 1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    newArray[array.Length] = newElement;
+    return newArray;
+}
+
+string[] newArray = new string[0];
 for (int i = 0; i < array.Length; i++)
 {
     if (array[i].Length <= 3)
     {
-        if (find)
-        {
-            Console.Write("\", \"");
-        }
-        else
-        {
-            find = true;
-        }
-        Console.Write(array[i]);
+        newArray = addElement(newArray, array[i]);
     }
 }
-Console.Write("\"]");
+Console.WriteLine($"Массив с элементами меньше 3-х символов:\n[\"{string.Join("\", \"", newArray)}\"]");
